db86c2a9d05ada27f673920357a1b4c4cd9ce724
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2023-11-27 13:32:19 +0300

    ww

--- 2.2/i.py
+++ 2.2/i.py
@@ -29,10 +29,4 @@
 n1 = input()
 n2 = input()
 n3 = input()
-
-if n1 < n2 and n1 < n3:
-    print(n1)
-elif n2 < n1 and n2 < n3:
-    print(n2)
-elif n3 < n1 and n3 < n2:
-    print(n3)    
+print(min(n1, n2, n3))
